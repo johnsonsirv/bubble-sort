3a07ac7a26497cc41efc9053b3ff715e53e9bf7a
Victor Johnson <johnsonsirv@gmail.com>
2019-05-28 15:23:13 +0200

    added code logic to compare values in the array and swap

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,9 +2,12 @@
 def bubble_sort(arr)
     upper_bound = arr.length-1
     upper_bound.downto(0) do |count|
-        puts "down #{indx}"
+        puts "down #{count}"
         (0...count).each do |indx|
-            puts "each #{indx}"
+            puts "comparing #{arr[indx]} #{arr[indx+1]}"
+            if arr[indx] > arr[indx+1]
+                arr[indx], arr[indx+1] = arr[indx+1], arr[indx]
+            end
         end
         puts "one pass"
     end
