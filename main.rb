1e6bab5199152a4a6bb78fb4ed08c632858e1793
Victor Johnson <johnsonsirv@gmail.com>
2019-05-28 16:16:12 +0200

    cls

--- main.rb
+++ main.rb
@@ -6,6 +6,5 @@ print bubble_sort([3,2,1,5,4])
 
 Benchmark.ips do |x|
     x.report("bubble_victor") { bubble_sort([3,2,1,5,4]) }
-    x.report("bubble_eche") { bubble_sort_eche([3,2,1,5,4]) }
     x.compare!
 end
