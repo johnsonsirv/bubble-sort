8ed5b9c666cbf0ded54b708753b7ea06201bfb22
Victor Johnson <johnsonsirv@gmail.com>
2019-05-28 16:17:57 +0200

    used test array data

--- main.rb
+++ main.rb
@@ -2,9 +2,3 @@ require './bubble_sort.rb'
 require "benchmark/ips" 
 
 print bubble_sort([3,2,1,5,4])
-
-
-Benchmark.ips do |x|
-    x.report("bubble_victor") { bubble_sort([3,2,1,5,4]) }
-    x.compare!
-end
