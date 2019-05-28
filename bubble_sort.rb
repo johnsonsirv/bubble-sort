c332c24baf0843dae48eaaa1c15e91f4c0d8154c
Victor Johnson <johnsonsirv@gmail.com>
2019-05-28 22:25:35 +0200

    fixed linting errors on whitespace indentations

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,31 +1,31 @@
 # bubble sort algorithm
 def bubble_sort(arr)
-    upper_bound = arr.length-1
-    upper_bound.downto(0) do |count|
-        swapped = false
-        (0...count).each do |indx|
-            if arr[indx] > arr[indx+1]
-                arr[indx], arr[indx+1] = arr[indx+1], arr[indx]
-                swapped = true
-            end
-        end
-        break unless swapped
+  upper_bound = arr.length-1
+  upper_bound.downto(0) do |count|
+    swapped = false
+    (0...count).each do |indx|
+      if arr[indx] > arr[indx+1]
+        arr[indx], arr[indx+1] = arr[indx+1], arr[indx]
+        swapped = true
+      end
     end
-    arr
+    break unless swapped
+  end
+  arr
 end
 
 # bubble_sort by
 def bubble_sort_by(arr)
-    upper_bound = arr.length-1
-    upper_bound.downto(0) do |count|
-        swapped = false
-        (0...count).each do |indx|
-            if yield(arr[indx], arr[indx+1]) > 0
-                arr[indx], arr[indx+1] = arr[indx+1], arr[indx]
-                swapped = true
-            end
-        end
-        break unless swapped
+  upper_bound = arr.length-1
+  upper_bound.downto(0) do |count|
+    swapped = false
+    (0...count).each do |indx|
+      if yield(arr[indx], arr[indx+1]) > 0
+        arr[indx], arr[indx+1] = arr[indx+1], arr[indx]
+        swapped = true
+      end
     end
-    arr
+    break unless swapped
+  end
+  arr
 end
