64aa9e115254e2e06516e55878ce58f48d3283b1
Victor Johnson <johnsonsirv@gmail.com>
2019-05-29 14:00:55 +0200

    added string literal using %w for word array

--- main.rb
+++ main.rb
@@ -2,10 +2,7 @@
 require './bubble_sort.rb'
 
 p bubble_sort([4, 3, 78, 2, 0, 2]).inspect
-sorted_array = bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+sorted_array = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
 p sorted_array
-
-sorted_array_desc = bubble_sort_by([4, 3, 2, 4, 7, 8, 9]) { |left, right| right - left }
-p sorted_array_desc
