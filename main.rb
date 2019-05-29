01ac45de53cccd4bc78d2b9043d2e2f0e110b754
Victor Johnson <johnsonsirv@gmail.com>
2019-05-29 09:54:43 +0200

    fixed whitespace

--- main.rb
+++ main.rb
@@ -6,5 +6,5 @@ sorted_array = bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
 end
 p sorted_array
 
-sorted_array_desc = bubble_sort_by([4, 3, 2, 4, 7, 8, 9]){ |left, right| right - left }
+sorted_array_desc = bubble_sort_by([4, 3, 2, 4, 7, 8, 9]) { |left, right| right - left }
 p sorted_array_desc
